2955380e0359257ea8dd53b44bdc42566499e92b
gombeee <snnnktf@yahoo.com>
2022-07-28 14:09:37 +0900

    commit

---
+++ process1.py
@@ -0,0 +1,48 @@
+# %%
+# 設問１
+# 監視ログファイルを読み込み、故障状態のサーバアドレスとそのサーバの故障期間を出力するプログラムを作成せよ。
+# 出力フォーマットは任意でよい。
+# なお、pingがタイムアウトした場合を故障とみなし、最初にタイムアウトしたときから、次にpingの応答が返るまでを故障期間とする。
+
+import re
+from datetime import datetime
+import pandas as pd
+
+with open('./sample.txt') as f:
+    logs=f.readlines()
+
+pattern=re.compile('(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2}),(.+/\d+),(.+)')
+failure=dict()  # {(故障中のipアドレス):(故障idまたは0)}のdict
+result=dict()  # {故障id:{ip:ipアドレス, start:故障開始時間, end:故障復旧時間}}のdict
+counter=10001
+
+for log in logs:
+    matches=pattern.match(log).groups()
+    ip,resp_time=matches[6:]
+    year,month,day,hour,min,sec=[int(s) for s in matches[:6]]
+    time=datetime(year,month,day,hour,min,sec)
+
+    # 故障の場合
+    if resp_time=='-': 
+        # 新たに故障した場合
+        if failure.get(ip,0)==0:
+            failure[ip]=counter
+            result[failure[ip]]={'ip':ip,'start':time,'end':None}
+            counter+=1
+
+    # 故障していない場合
+    elif int(resp_time)>=0:
+        # 故障から復旧した場合
+        if failure.get(ip,0)!=0:
+            result[failure[ip]]['end']=time
+            failure[ip]=0
+
+    else:
+         raise ValueError()
+
+    data=pd.DataFrame.from_dict(result,orient='index')
+    data.to_csv('./output.tsv',sep='\t')
+
+    
+
+# %%
---
+++ process2.py
@@ -0,0 +1,59 @@
+# %%
+# 設問２
+# ネットワークの状態によっては、一時的にpingがタイムアウトしても、一定期間するとpingの応答が復活することがあり、
+# そのような場合はサーバの故障とみなさないようにしたい。
+# N回以上連続してタイムアウトした場合にのみ故障とみなすように、設問1のプログラムを拡張せよ。
+# Nはプログラムのパラメータとして与えられるようにすること。
+
+import re
+from datetime import datetime
+import pandas as pd
+
+with open('./sample.txt') as f:
+    logs=f.readlines()
+
+N=2
+
+pattern=re.compile('(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2}),(.+/\d+),(.+)')
+status=dict()  # {(ipアドレス):(連続タイムアウト回数)}のdict
+timeout=dict()  # {(ipアドレス):(timeout開始時刻または0)}のdict
+failure=dict()  # {(ipアドレス):(故障idまたは0)}のdict
+result=dict()  # {故障id:{ip:ipアドレス, start:故障開始時間, end:故障復旧時間}}のdict
+counter=10001
+
+for log in logs:
+    matches=pattern.match(log).groups()
+    ip,resp_time=matches[6:]
+    year,month,day,hour,min,sec=[int(s) for s in matches[:6]]
+    time=datetime(year,month,day,hour,min,sec)
+
+    # 故障の場合
+    if resp_time=='-': 
+        status[ip]=status.get(ip,0)+1
+        # 新たに故障した場合
+        if status.get(ip,0)>=N:
+            failure[ip]=counter
+            result[failure[ip]]={'ip':ip,'start':time,'end':None}
+            counter+=1
+        # 新たにタイムアウトし出した場合
+        elif status.get(ip,0)==1:
+            
+
+    # 故障していない場合
+    elif int(resp_time)>=0:
+        # タイムアウトから復旧した場合
+        if timeout.get(ip,0)!=0:
+            
+            if failure.get(ip,0)!=0:
+                result[failure[ip]]['end']=time
+                failure[ip]=0
+
+    else:
+         raise ValueError()
+
+    data=pd.DataFrame.from_dict(result,orient='index')
+    data.to_csv('./output.tsv',sep='\t')
+
+    
+
+# %%
